46d831f6c0f2eabafa679359be11f6ed0804efbe
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-19 17:31:12 -0300

    interagindo com botão e com click

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -6,8 +6,21 @@ describe('Cypress Basics', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento') 
             .and('contain', 'Campo')
+        
+        //TODO Escrever title no console
+        //TODO Escrever o title em um campo de testo
+
     })
 
-    //TODO Escrever title no console
-    //TODO Escrever o title em um campo de testo
+   
+
+    it.only('Should find and interact with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+
+    })
+
+
 })
