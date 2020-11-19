08bab9a3b2f0f45c6b30da07bafa461f3f8bf237
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-19 16:05:01 -0300

    promises

---
+++ cypress/integration/promises.spec.js
@@ -0,0 +1,23 @@
+it('sem testes ainda ...', () => {})
+
+const getSomething = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(11) 
+        }, 1000) 
+    })
+   
+}
+
+
+const system = () => {
+    console.log('init')
+    const prom = getSomething()
+    prom.then(some => {
+        console.log(`Something is ${some}`)
+    })
+    console.log('end')
+    
+}
+
+system()
