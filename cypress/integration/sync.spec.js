b2a7445335dc0e04f8e7cb7d00627351b48bec8e
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-20 16:06:31 -0300

    alterar wait e timeout padrao

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -16,17 +16,36 @@ describe('Waits...', () => {
         cy.get("#novoCampo").should('exist')
     })
 
-    it.only('Deve fazer retrys', () => {
+    it('Deve fazer retrys', () => {
         cy.get('#buttonDelay').click()
         cy.get("#novoCampo").should('not.exist').should('exist')
     })
 
-    it.only('Uso do find', () => {
+    it('Uso do find', () => {
         cy.get('#buttonList').click()
         cy.get('#lista li')
             .find('span').should('contain', 'Item 1')
         cy.get('#lista li span')
-        .should('contain', 'Item 2')
+            .should('contain', 'Item 2')
+    })
+
+    it.only('Alterando wait e timeout', () => {
+        //cy.get('#buttonDelay').click()
+        //cy.get("#novoCampo", {timeout: 1000}).should('exist')
+        
+        //cy.get('#buttonListDOM').click()
+        //cy.wait(5000)
+        //cy.get('#lista li span', {timeout: 6000})
+        //.should('contain', 'Item 2')
+
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span', {timeout: 6000})
+            .should('have.length', 1)
+        
+
+
+
+
     })
 
 })
