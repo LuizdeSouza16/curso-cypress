37586c066f9481ee57a054349cd619fdc1762789
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-23 15:42:59 -0300

    each

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -1,5 +1,6 @@
 /// <reference types = "cypress" />
 
+
 describe('Dinamic test', () => {
     before(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
@@ -27,6 +28,25 @@ describe('Dinamic test', () => {
             cy.get('#resultado').should('contain', "Cadastrado!")
         })
     })
+
+    it.only("Deve selecionar todos usando o each", () => {
+        cy.get('#formNome').type("Luiz")
+        cy.get('#formSobrenome').type('Souza')
+        cy.get(`[name=formSexo][value=M]`).click()
+        cy.get('[name=formComidaFavorita]').each($el => {
+            if($el.val() !== 'vegetariano'){
+                cy.wrap($el).click()
+            }
+                
+        })
+        cy.get('#formEscolaridade').select("Doutorado")
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado').should('contain', "Cadastrado!")
+        //cy.clickAlert('#formCadastrar','Tem certeza que voce eh vegetariano?')
+    })
+
+    
 
   
 })
