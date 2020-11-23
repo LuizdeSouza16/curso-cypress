16ccafea4d483ac35efff0f03bc72d4f82db2972
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-23 10:02:23 -0300

    confirm

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -25,4 +25,31 @@ describe('Work with alerts' , ()=> {
         
     })
 
+    it.only('Confirm', () => {
+        cy.get('#confirm')
+            .click()
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+    })
+
+    it.only('Deny', () => {
+        cy.get('#confirm')
+            .click()
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+    })
+
 })
