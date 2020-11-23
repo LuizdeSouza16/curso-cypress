e53f261fc82488b7fe6dd776e0d7b44d347e6a88
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-23 10:18:27 -0300

    prompt

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -52,4 +52,22 @@ describe('Work with alerts' , ()=> {
         })
     })
 
+    it.only('Prompt', () => {
+       
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('42')
+        })
+        
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Era 42?')
+        })
+
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+
+        cy.get('#prompt')
+        .click()
+    })
+
 })
