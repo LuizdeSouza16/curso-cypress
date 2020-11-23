55416e2ce0037cf636464db4b55222d5221e6691
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-23 13:36:45 -0300

    locator jquery

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -5,8 +5,11 @@ describe('Locators', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
     })
 
-    it('...', () => {
-
+    it('using jquery selector', () => {
+        cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
+        cy.get('[onclick*="Francisco"]').click()
+        cy.get("#tabelaUsuarios td:contains('Doutorado'):eq(0) ~ td:eq(3)").type('Olá')
+        cy.get("#tabelaUsuarios tr:contains('Doutorado'):eq(0) td:eq(6)")
     })
 
 })
