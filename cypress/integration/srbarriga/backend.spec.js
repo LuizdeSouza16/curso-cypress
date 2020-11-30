fce8304bca266e6238de78c5db01da417b40de42
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-30 17:40:40 -0300

    alterar uma conta via rest

--- cypress/integration/srbarriga/backend.spec.js
+++ cypress/integration/srbarriga/backend.spec.js
@@ -16,21 +16,47 @@ describe('Should test at backend test..', () => {
     })
 
     it('Should a create an account', () => {
-       
-    cy.request({
-        url: '/contas',
-        method: 'POST',
-        headers: {Authorization: `JWT ${token}`},
-        body: {
-            nome: "Conta via rest"
-        },
-    }).as('response')
+        cy.request({
+            url: '/contas',
+            method: 'POST',
+            headers: {Authorization: `JWT ${token}`},
+            body: {
+                nome: "Conta via rest"
+            },
+        }).as('response')
 
-    cy.get('@response').then(res => {
-        expect(res.status).to.be.equal(201)
-        expect(res.body).to.have.property('id')
-        expect(res.body).to.have.property('nome','Conta via rest')
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome','Conta via rest')
         })
     })
+
+    it('Should update an account', () => {
+        
+
+        cy.request({
+            url: '/contas',
+            method: 'GET',
+            headers: {Authorization: `JWT ${token}`},
+            qs: {
+                nome: 'Conta para alterar'
+            }
+
+        }).then(res => {
+            cy.request({
+                url: `https://barrigarest.wcaquino.me/contas/${res.body[0].id}`,
+                method: 'PUT',
+                headers: {Authorization: `JWT ${token}`},
+                body: {
+                    nome: 'conta alterada via rest'
+                }
+            }).as('response')
+        })
+
+       cy.get('@response').its('status').should('be.equal', 200)
+    })
+    
+
 
 })
