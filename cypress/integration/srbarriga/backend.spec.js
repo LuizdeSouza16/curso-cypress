fe2071ff3fea46fbd14c9bcca1df38c3ab3ba88d
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-30 16:56:34 -0300

    criar uma conta via request

--- cypress/integration/srbarriga/backend.spec.js
+++ cypress/integration/srbarriga/backend.spec.js
@@ -10,7 +10,7 @@ describe('Should test at backend test..', () => {
         
     })
 
-    it('Should a create a account', () => {
+    it('Should a create an account', () => {
         cy.request({
             method: 'POST',
             url: 'https://barrigarest.wcaquino.me/signin',
@@ -19,9 +19,32 @@ describe('Should test at backend test..', () => {
                 redirecionar: false,
                 senha: '12345678'
             }
+           
             
         }).its('body.token').should('not.be.empty')
+        .then(token => {
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/contas',
+                method: 'POST',
+                headers: {Authorization: `JWT ${token}`},
+                body: {
+                    nome: "Conta via rest"
+                },
+            }).as('response')
+        })
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome','Conta via rest')
+        })
     })
+
+
+    it('Insert an account', () => {
+        
+    }) 
+
+
 
     
 
